f476dc11b57f2e41ecd4f0229b4ebc470755a1ca
Helegbedey <devaze007@gmail.com>
2020-02-09 13:05:02 +0100

    jnks

--- src/Component/afterAuth/dashboard/case.js
+++ src/Component/afterAuth/dashboard/case.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {Container} from 'react-bootstrap';
+import {Container, Button} from 'react-bootstrap';
 import axios from 'axios';
 
 class Case extends Component {
     state={
-        data: null
+        data: []
 
     }
     componentDidMount(){
@@ -13,26 +13,51 @@ class Case extends Component {
     async getDataAxios(){
         const proxy = "https://cors-anywhere.herokuapp.com/";
         const response =
-          await axios.get(`${proxy}https://cacerem.herokuapp.com/inmate/get_inmate?inmateId=5`)
+          await axios.get(`${proxy}https://cacerem.herokuapp.com/inmate/get_inmate?inmateId=11`)
           this.setState({
               data: response.data
           })
-        console.log(this.state.data)
+        console.log(this.state.data.name)
+        console.log(response.data)
     }
+    OnGetALawyerClick = () => {
+        this.getALawyer()
+    }
+     getALawyer = event =>{
+    const inmateDetails = {
+        details:{
+            id: this.state.data.id
+        }
+    }
+
+       const proxy = "https://cors-anywhere.herokuapp.com/";
+       const response = axios.post(`${proxy}https://cacerem.herokuapp.com/inmate/assign_lawyer`, { inmateDetails })
+       .then(res => {
+         console.log(response);
+         console.log(res.data);
+       })
+    }
+    
    render(){
+
     return(
       <Container>
            <div className='case-details'>
            <h3>Case Details</h3>
            <div className='case-content'>
                <div className='case-status'>
-               <h6>Status : <span>Active</span></h6>
                </div>
                <div className='case-owner'>
-    <h6>Name: <span></span></h6>
+    <h6>Name: <span>{this.state.data.name}</span></h6>
                    <h6>Case ID: <span>cdca0aa2c68290eb41f7d3e77f6669c6</span></h6>
-                   <h6>Case Description: <span>Lorem Ipsum</span></h6>
+                   <h6>Alleged Crime: <span>{this.state.data.allegedCrime}</span></h6>
+    <h6>Case Description: <span>{this.state.data.crimeDescription}</span></h6>
+    <h6>Inmate Status : <span>{this.state.data.inmateStatus}</span></h6>
                </div>
+
+               <div className='get-a-lawyer'>
+                   <Button type='submit' onClick={this.OnGetALawyerClick}>Get a lawyer</Button>
+                   </div>
 
            </div>
            </div>
